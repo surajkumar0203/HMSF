4ef53af89c9d8685c2ff156ff4198e8534173687
Suraj Kumar <kumarsuraj0203@gmail.com>
2025-08-01 20:10:39 +0530

    Add .scripts and .github for automate. Change in master branch

---
+++ .scripts/react_deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app
+echo "Copying New changes...."
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25520" git pull
+
+echo "New changes copied to server !"
+
+echo "Go To HMS"
+cd HMS
+
+echo "Installing Dependencies..."
+npm init --yes
+
+
+echo "Creating Production Build..."
+npm run build
+
+echo "Deployment Finished !"
+
+ 
